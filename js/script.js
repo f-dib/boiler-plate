1384ce6ea4f7eed4562d5e513e9a082f73e7c9b7
federico-dibitonto <f.dibitonto94@gmail.com>
2024-01-31 23:11:09 +0100

    exercise completed + EXPERIMENTS

--- js/script.js
+++ js/script.js
@@ -2,34 +2,40 @@ const gridElement = document.querySelector("#grid")
 
 for(let i = 1; i <= 100; i++) {
 
-    const newElement = document.createElement("div");
-    newElement.className = "square";
-    newElement.innerHTML = i;
-    newElement.style.fontSize = "20px";
-    newElement.style.fontWeight = "bold";
-    newElement.style.color = "black";
-    newElement.style.backgroundColor = "#1389b2";
+
+    //EXPERIMENT: I set a delay of 100 milliseconds in the creation of each square
     
-
-
-    if (i % 3 == 0){
-
-        newElement.innerHTML = 'FIZZ';
-        newElement.style.backgroundColor = '#0cd6a1'
-
-    } else if (i % 5 == 0){
-
-        newElement.innerHTML = 'BUZZ';
-        newElement.style.backgroundColor = '#ffd166';
+    setTimeout(function() {
+        
+        const newElement = document.createElement("div");
+        newElement.className = "square";
+        newElement.innerHTML = i;
+        newElement.style.fontSize = "20px";
+        newElement.style.fontWeight = "bold";
+        newElement.style.color = "black";
+        newElement.style.backgroundColor = "#1389b2";
     
-    }
+        if (i % 3 == 0){
     
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-
-        newElement.innerHTML = 'FIZZBUZZ';
-        newElement.style.backgroundColor = '#f0466f';
+            newElement.innerHTML = 'FIZZ';
+            newElement.style.backgroundColor = '#0cd6a1'
     
-    }
+        } else if (i % 5 == 0){
+    
+            newElement.innerHTML = 'BUZZ';
+            newElement.style.backgroundColor = '#ffd166';
+        
+        }
+        
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+    
+            newElement.innerHTML = 'FIZZBUZZ';
+            newElement.style.backgroundColor = '#f0466f';
+        
+        }
+    
+        gridElement.append(newElement);
 
-    gridElement.append(newElement);
+    },i * 200)
+
 }
