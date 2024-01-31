d6645eb5995feded8a36d85655bcc4d0d3c66ea8
federico-dibitonto <f.dibitonto94@gmail.com>
2024-01-31 17:23:49 +0100

    bonus 1 completed

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const gridElement = document.querySelector("#grid")
 
 for(let i = 1; i <= 100; i++) {
 
-    gridElement.innerHTML += `<div class="square">${i}</div>`;
+    const newElement = document.createElement("div");
+    newElement.className = "square";
+    newElement.innerHTML = i;
+
+    gridElement.append(newElement);
+    
     
 }
