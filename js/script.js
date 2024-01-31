608ee88915ec7bb3b2dc9b89adf17d49b547f6b6
federico-dibitonto <f.dibitonto94@gmail.com>
2024-01-31 18:04:39 +0100

    bonus 2 completed

--- js/script.js
+++ js/script.js
@@ -5,8 +5,31 @@ for(let i = 1; i <= 100; i++) {
     const newElement = document.createElement("div");
     newElement.className = "square";
     newElement.innerHTML = i;
+    newElement.style.fontSize = "20px";
+    newElement.style.fontWeight = "bold";
+    newElement.style.color = "black";
+    newElement.style.backgroundColor = "#1389b2";
+    
+
+
+    if (i % 3 == 0){
+
+        newElement.innerHTML = 'FIZZ';
+        newElement.style.backgroundColor = '#0cd6a1'
+
+    } else if (i % 5 == 0){
+
+        newElement.innerHTML = 'BUZZ';
+        newElement.style.backgroundColor = '#ffd166';
+    
+    }
+    
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+
+        newElement.innerHTML = 'FIZZBUZZ';
+        newElement.style.backgroundColor = '#f0466f';
+    
+    }
 
     gridElement.append(newElement);
-    
-    
 }
